4ac591845da9e413ed9e0f238524de2a37591a99
Paweł Twardowski <p.twardowski03@gmail.com>
2025-03-12 22:46:00 +0100

    Zrobiłem licznik kroków, timer, zrobiony dystans, dodałem jakis cel kroków, przycisk pauzy, progress bar. Powinno też pobierać dane z sensorów. Nie testowałem bo UI nie mam do podpięcia do tego.

--- app/src/main/java/com/example/projektmovexp/MainActivity.java
+++ app/src/main/java/com/example/projektmovexp/MainActivity.java
@@ -1,6 +1,17 @@
 package com.example.projektmovexp;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,26 +23,139 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.projektmovexp.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private TextView stepCountTextView;
+    private TextView distanceTextView;
+    private TextView timeTextView;
+    private Button pauseButton;
+    private SensorManager sensorManager;
+    private Sensor stepCounterSensor;
+    private int stepCount = 0;
+    private long timePaused = 0;
+    private long startTime;
+    private Handler timerHandler = new Handler();
+    private  Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            long milliseconds = System.currentTimeMillis() - startTime;
+            long seconds = milliseconds / 1000;
+            long minutes = seconds / 60;
+            long hours = minutes / 60;
+            timeTextView.setText(String.format(Locale.getDefault(), "Time: %02d:%02d:%02d", hours % 60, minutes % 60, seconds % 60));
+            timerHandler.postDelayed(this, 1000);
+
+
+        }
+    };
+
+    private boolean isPaused = false;
+    private float stepLength = 0.7f;
+    private int stepCountGoal = 5000;
+    private TextView stepCountGoalTextView;
+    private ProgressBar progressBar;
     private ActivityMainBinding binding;
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        if(stepCounterSensor!=null){
+            sensorManager.unregisterListener(this);
+            timerHandler.removeCallbacks(timerRunnable);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (stepCounterSensor != null) {
+            sensorManager.registerListener(this, stepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+            timerHandler.postDelayed(timerRunnable, 0);
+        }
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // to jest do podłączenie do ui z activity_main.xml
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        // to zakomentowałem bo nie wiem jak dodac do poszczególnych zakładek z szablonu
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // to są pola tekstowe, przycisk, progress bar który powinien byc w ui, nie dziala bo ui nie miałem zeby polaczyc!!!!!!!!!!
+        // reszta powinna dzialac na oko, nie wiem nie testowalem bo ui nie chce mi sie robic
+
+        stepCountTextView = findViewById(R.id.stepCountTextView);
+        distanceTextView = findViewById(R.id.distanceTextView);
+        timeTextView = findViewById(R.id.timeTextView);
+        pauseButton = findViewById(R.id.pauseButton);
+        stepCountGoalTextView = findViewById(R.id.stepCountGoalTextView);
+        stepCountGoalTextView.setText("Goal: " + stepCountGoal + " steps");
+        progressBar = findViewById(R.id.progressBar);
+
+        startTime = System.currentTimeMillis();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        progressBar.setMax(stepCountGoal);
+        stepCountGoalTextView.setText("Goal: " + stepCountGoal + " steps");
+
+        if(stepCounterSensor == null) {
+            stepCountTextView.setText("Step Counter Sensor not available");
+
+        }
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            stepCount = (int) sensorEvent.values[0];
+            stepCountTextView.setText("Step Count: " + stepCount);
+            progressBar.setProgress(stepCount);
+            if (stepCount >= stepCountGoal) {
+                stepCountTextView.setText("Congratulations! You reached your goal!");
+            }
+            float distanceInKm = stepCount * stepLength / 1000;
+            distanceTextView.setText(String.format(Locale.getDefault(),"Distance: %.2f km",distanceInKm));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        // nie wiem do czego to służy ale chyba działa gdy jest puste
+    }
+
+    public void onPausedButtonClicked(View view){
+        if (isPaused){
+            isPaused = false;
+            pauseButton.setText("Pause");
+            startTime = System.currentTimeMillis() - timePaused;
+            timerHandler.postDelayed(timerRunnable,0);
+        }else{
+            isPaused=true;
+            pauseButton.setText("Resume");
+
+            timerHandler.removeCallbacks(timerRunnable);
+            timePaused = System.currentTimeMillis() - startTime;
+        }
     }
 
 }
